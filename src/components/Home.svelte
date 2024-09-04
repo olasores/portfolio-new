5c85f20693f1892e7a60109aaf5bef0a67769dfb
Ola <oyinkansola_o@yahoo.com>
2024-09-04 13:54:01 -0700

    added about me

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -8,11 +8,11 @@
     import olaimg from "../lib/assets/olaimg.png"
 </script>
 
-<main class="flex justify-center items-center h-screen">
-    <div class="flex items-center space-x-8">
+<main class=" flex flex-col items-start justify-center mx-auto max-w-[700px] p-40 px-10 pb-4 pt-16 text-white ">
+    <div class="flex items-center space-x-8 mb-9">
         <div class="">
             <!-- img here -->
-             <img src="{olaimg}" alt="ola-img">
+             <img src="{olaimg}" class="f"  alt="ola-img">
         </div>
         <div class="text-white ">
             <!-- words here -->
@@ -20,13 +20,38 @@
              <p >Front End Developer</p>
 
              <div class="flex space-x-2">
-                <a class="bg-blue-200 px-2 py-2 rounded" target="_blank" href="https://github.com/olasores">Github</a>
-                <a class="bg-blue-200 px-2 py-2 rounded" target="_blank" href="https://www.linkedin.com/in/oyinkansola-olayinka/">Linkedin</a>
+                <!-- might be better to add icons here -->
+                <a class="bg-[#A8A8A8] px-1 py-1 rounded" target="_blank" href="https://github.com/olasores">Github</a>
+                <a class="bg-[#A8A8A8] px-1 py-1 rounded" target="_blank" href="https://www.linkedin.com/in/oyinkansola-olayinka/">Linkedin</a>
              </div>
              
 
         </div>
     </div>
+
+    <div class="mt-9">
+        <h1 class="text-2xl mb-0">About Me</h1>
+        <!-- <p class="mt-0 p-0 mb-2 text-[#A8A8A8]"> A little About me</p> -->
+         <p class="text-[#A8A8A8]">My name is Ola, a junior studying computer science at California state university Fullerton. I enjoy listening to music, and reading during my free time. 
+            I am Interested in various fields of technology which include but not limited to: UX/UI design, Front end development and Data Science. </p>
+    </div>
+   
+    <div class="mt-9">
+        <h1 class="text-2xl">Projects </h1>
+        <!-- <p> some of my works </p> -->
+    </div>
+
+
+    
+    <!-- <div class="mt-9">
+        <h1 class="text-2xl">Work Experience </h1>
+    </div> -->
+
+    <!-- <div class="mt-9">
+        <h1 class="text-2xl">Education </h1>
+    </div> -->
+
+
 
 </main>
 
