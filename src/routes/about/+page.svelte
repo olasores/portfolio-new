ae5609a49682c668dec515fc109be9925b32497a
Ola <oyinkansola_o@yahoo.com>
2024-09-18 18:29:16 -0700

    links work

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import About from "../../components/About.svelte";
+    import NavBar from "../../components/NavBar.svelte";
+</script>
+
+<main>
+    <NavBar />
+    <About />
+</main>
