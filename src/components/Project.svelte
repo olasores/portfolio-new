c4a51b6e7b3a925fcd28df5956e5db21884b292d
Ola <oyinkansola_o@yahoo.com>
2024-11-15 11:16:36 -0800

    fixed project page

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -1,14 +1,20 @@
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Judson:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+</svelte:head>
 
 <script>
+    import { darkMode } from "../stores.js";
 
 </script>
 
-
-<main class="mx-auto max-w-[700px] p-40 px-10 pb-4 pt-16 text-white w-full">
+<main class="mx-auto max-w-[700px] p-40 px-10 pb-4 pt-16 text-white w-full  {$darkMode
+    ? 'text-white bg-black'
+    : 'text-white-mode-900 bg-white-mode-50'}">
     <h1>This is going to be the project page</h1>
 </main>
 
-
 <style>
-
+    
 </style>
