b98220241d7333da151c4d3f5d76d514b9b07cf5
Ola <oyinkansola_o@yahoo.com>
2024-11-15 11:02:23 -0800

    idk anymore

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,6 +1,7 @@
 <script>
     import { f } from "feather-icons";
     // using https://feathericons.com/
+    // https://github.com/dylanblokhuis/svelte-feather-icons
 
     // icons 
     import { MoonIcon} from 'svelte-feather-icons'
@@ -28,12 +29,13 @@
     
 
 </script>
-
+<!-- 
 <main class="mx-auto max-w-[700px] p-40 px-10 pb-4 pt-16 {$darkMode
 ? 'text-white'
 : 'text-white-mode-900 bg-white-mode-50'}">
+
     <nav class="flex item-center space-x-40">
-        <!-- logo -->
+        logo
         <div class="">
             <img src="/assets/logo.png" alt="o-logo">
         </div>
@@ -60,6 +62,42 @@
       
 
     </nav>
+
+    
+
+</main> -->
+
+
+<main class="mx-auto max-w-[700px] p-40 px-10 pb-4 pt-16 {$darkMode 
+? 'text-white' 
+: 'text-white-mode-900 bg-white-mode-50'}">
+
+  <nav class="flex items-center justify-between space-x-40">
+    <!-- Logo (Hidden on Mobile) -->
+    <div class="hidden sm:block">
+      <img src="/assets/logo.png" alt="o-logo">
+    </div>
+
+    <!-- Navigation Links -->
+    <ul class="flex flex-1 justify-center sm:justify-start space-x-4 sm:space-x-6">
+      {#each navBar as item}
+        <li>
+          <a href={item.link} class="hover:text-gray-400 transition-colors duration-300">
+            {item.name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+
+    <!-- Dark Mode Toggle -->
+    <button on:click={toggle} class="focus:outline-none">
+      {#if $darkMode}
+        <SunIcon size="1.5x" class="text-[#A8A8A8]" />
+      {:else}
+        <MoonIcon size="1.5x" class="text-[#A8A8A8]" />
+      {/if}
+    </button>
+  </nav>
 
 </main>
 
